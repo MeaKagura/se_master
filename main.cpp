b48dc4b3ab7eb7e8717b25e4edd3f6e48575334b
makisekurisu <zhaoqixuan@ustc.edu>
2023-03-12 16:34:08 +0800

    Changes to be committed: 	new file:   .vscode/settings.json 	modified:   main 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@ using namespace std;
 int main() {
 	time_t start = clock();
     cout << "hello, world!" << endl;
+	srand(time(0));
+	int num = 0;
+	for (int i = 0; i < 1e6; i++) {
+		num += num < 1000 ? rand() % 3 : -(rand() % 3);
+	}
+	cout << num << endl;
 	time_t end = clock();
 	cout << "运行时间：" << (end - start) / (CLOCKS_PER_SEC / 1000) << " ms" << endl;
     return 0;
